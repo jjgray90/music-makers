6a6501d0a8dcedb8f5a703c94452d0c5f505795c
Your Name <jjgray90@gmail.com>
2025-11-10 18:49:33 +0000

    nav height

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,7 +16,7 @@ export function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <nav className="border-b border-gray-200 bg-white/60 backdrop-blur-md sticky top-0 z-50">
+    <nav className="h-[10vh] border-b border-gray-200 bg-white/60 backdrop-blur-md sticky top-0 z-50">
       <div className=" max-w-6xl mx-auto flex items-center justify-between px-6 py-4">
         <Link
           href="/"
